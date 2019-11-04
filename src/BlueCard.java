29f8ae0fd6b834449455941bb99ff0cce43639b5
AliceXu9404 <56173195+AliceXu9404@users.noreply.github.com>
2019-11-04 13:58:03 -0600

    Update BlueCard.java

--- src/BlueCard.java
+++ src/BlueCard.java
@@ -3,23 +3,18 @@ import java.util.ArrayList;
 public class BlueCard extends Card
 {
     private ArrayList<String> cost;
-    private String prerequisite;
     private ArrayList<String> chains;
     private int victoryPoints;
 
-    public BlueCard(String name, String color, ArrayList<String> cost, String prerequisite, ArrayList<String> chains, int victoryPoints) {
+    public BlueCard(String name, String color, ArrayList<String> cost, ArrayList<String> chains, int victoryPoints) {
         super(name, color);
         this.cost = cost;
-        this.prerequisite = prerequisite;
         this.chains = chains;
         this.victoryPoints = victoryPoints;
     }
 
     public ArrayList<String> getCost() {
         return cost;
-    }
-    public String getPrerequisite() {
-        return prerequisite;
     }
     public ArrayList<String> getChains() {
         return chains;
