8ad8e231048541d2d7b1a344409ae4b2c33f85df
RobertWay <56172985+RobertWay@users.noreply.github.com>
2019-11-17 21:38:23 -0600

    Add files via upload

--- src/Deck.java
+++ src/Deck.java
@@ -1,6 +1,7 @@
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Scanner;
 
@@ -13,8 +14,6 @@ public class Deck {
         Scanner textFileScan = new Scanner(new File("age1Cards.txt"));
         String[] tempArray = new String[7];
 
-        discard = new ArrayList<Card>();
-
         while(textFileScan.hasNextLine()) {
             tempArray = textFileScan.nextLine().split("|");
             if(tempArray[0].equals("gray")) {
@@ -24,44 +23,65 @@ public class Deck {
                 String color = tempArray[0];
                 String name = tempArray[1];
                 int cost = Integer.parseInt(tempArray[3]);
+
                 ArrayList<String> resources = new ArrayList<String>();
                 resources.add(tempArray[2]);
+
                 tempArrayList.add(new BrownCard(color, name, cost, resources));
             }
             else if(tempArray[0].equals("green")) {
                 String color = tempArray[0];
                 String name = tempArray[1];
                 String science = tempArray[2];
-                ArrayList<String> cost = new ArrayList<String>();
-                cost.add(tempArray[3]);
                 String chain1 = tempArray[4];
                 String chain2 = tempArray[5];
+
+                ArrayList<String> cost = new ArrayList<String>();
+                cost.add(tempArray[3]);
 
                 tempArrayList.add(new GreenCard(color, name, cost, science, chain1, chain2));
             }
             else if(tempArray[0].equals("red")) {
                 String color = tempArray[0];
                 String name = tempArray[1];
-                int military = Integer.parseInt(tempArray[3]);
-                String chain1 = tempArray[5];
-                String chain2 = tempArray[6];
+                int military = Integer.parseInt(tempArray[2]);
+                String chain1 = tempArray[3];
+                String chain2 = "";
 
                 ArrayList<String> cost = new ArrayList<String>();
-                tempArray = tempArray[3].split("&");
-                for(int i = 0; i < tempArray.length; i++)
-                    cost.add(tempArray[i]);
 
                 tempArrayList.add(new RedCard(color, name, cost, military, chain1, chain2));
             }
             else if(tempArray[0].equals("yellow")) {
+                String color = tempArray[0];
+                String name = tempArray[1];
+                String chain1 = tempArray[3];
 
+                tempArray = tempArray[2].split(",");
+
+                String action = tempArray[0];
+                int coins = 1;
+
+                boolean left = false;
+                if(tempArray[2].equals("true"))
+                    left = true;
+                boolean right = false;
+                if(tempArray[3].equals("true"))
+                    right = true;
+
+                ArrayList<String> resources = new ArrayList<String>();
+                tempArray = tempArray[1].split(".");
+                for(String resouce : tempArray)
+                    resources.add(resouce);
+
+                tempArrayList.add(new YellowCard(color, name, action, resources, coins, left, right, chain1));
             }
             else if(tempArray[0].equals("blue")) {
                 String color = tempArray[0];
                 String name = tempArray[1];
-                int victoryPoints = Integer.parseInt(tempArray[3]);
-                String chain1 = tempArray[5];
-                String chain2 = tempArray[6];
+                int victoryPoints = Integer.parseInt(tempArray[2]);
+                String chain1 = tempArray[3];
+                String chain2 = "";
 
                 ArrayList<String> cost = new ArrayList<String>();
                 tempArray = tempArray[3].split("&");
@@ -71,10 +91,27 @@ public class Deck {
                 tempArrayList.add(new BlueCard(color, name, cost, victoryPoints, chain1, chain2));
             }
         }
+        Collections.shuffle(tempArrayList);
+        cards = new HashMap<Integer, ArrayList<Card>>();
+
+        ArrayList<Card> tempHand = new ArrayList<Card>();
+        for(int i = 0; i < 7; i++)
+            tempHand.add(tempArrayList.get(i));
+        cards.put(1, tempHand);
+        tempHand = new ArrayList<Card>();
+        for(int i = 7; i < 14; i++)
+            tempHand.add(tempArrayList.get(i));
+        cards.put(1, tempHand);
+        tempHand = new ArrayList<Card>();
+        for(int i = 14; i < 20; i++)
+            tempHand.add(tempArrayList.get(i));
+        cards.put(1, tempHand);
+
+        discard = new ArrayList<Card>();
     }
 
-    public HashMap<Integer, ArrayList<Card>> getCards() {
-        return cards;
+    public ArrayList<Card> getHand(int val) {
+        return cards.get(val);
     }
     public ArrayList<Card> getDiscard() {
         return discard;
@@ -83,10 +120,6 @@ public class Deck {
     public void discard(Card card) {
         discard.add(card);
     }
-    public ArrayList<Card> getHand(int val) {
-        return cards.get(val);
-    }
-
     public void incrementAge() {
 
     }
--- src/YellowCard.java
+++ src/YellowCard.java
@@ -3,27 +3,42 @@ import java.util.ArrayList;
 public class YellowCard extends Card {
     private ArrayList<String> cost;
     private String action;
-    private String cardColor;
-    private int coins;
-    private int victoryPoints;
     private ArrayList<String> resources;
+    private int coins;
     private boolean left;
     private boolean right;
-    String chain1 = "";
-    String chain2 = "";
+    private String awardColor;
+    private int victoryPoints;
+    private String chain1;
+    private String chain2;
 
-    public YellowCard(String color, String name, ArrayList<String> cost, String action, String cardColor, int coins, int victoryPoints, ArrayList<String> resources, boolean left, boolean right, String chain1, String chain2) {
+    public YellowCard(String color, String name, ArrayList<String> cost, String action, ArrayList<String> resources, int coins, boolean left, boolean right, String awardColor, int victoryPoints, String chain1, String chain2) {
         super(color, name);
         this.cost = cost;
         this.action = action;
-        this.cardColor = cardColor;
-        this.coins = coins;
-        this.victoryPoints = victoryPoints;
         this.resources = resources;
+        this.coins = coins;
+        this.left = left;
+        this.right = right;
+        this.awardColor = awardColor;
+        this.victoryPoints = victoryPoints;
+        this.chain1 = chain1;
+        this.chain2 = chain2;
+    }
+
+    public YellowCard(String color, String name, String action, ArrayList<String> resources, int coins, boolean left, boolean right, String chain1) {
+        super(color, name);
+        this.action = action;
+        this.resources = resources;
+        this.coins = coins;
         this.left = left;
         this.right = right;
         this.chain1 = chain1;
-        this.chain2 = chain2;
+
+        cost = new ArrayList<String>();
+        awardColor = "";
+        victoryPoints = 0;
+        chain2 = "";
     }
 
     public ArrayList<String> getCost() {
@@ -32,23 +47,23 @@ public class YellowCard extends Card {
     public String getAction() {
         return action;
     }
-    public String getCardColor() {
-        return cardColor;
+    public ArrayList<String> getResources() {
+        return resources;
     }
     public int getCoins() {
         return coins;
-    }
-    public int getVictoryPoints() {
-        return victoryPoints;
-    }
-    public ArrayList<String> getResources() {
-        return resources;
     }
     public boolean isLeft() {
         return left;
     }
     public boolean isRight() {
         return right;
+    }
+    public String getAwardColor() {
+        return awardColor;
+    }
+    public int getVictoryPoints() {
+        return victoryPoints;
     }
     public String getChain1() {
         return chain1;
