a698fc5395a37c881af2e3407f4838e93293e0a6
AliceXu9404 <56173195+AliceXu9404@users.noreply.github.com>
2019-11-27 15:15:47 -0600

    Update Wonder.java

--- src/Wonder.java
+++ src/Wonder.java
@@ -2,6 +2,33 @@ import java.util.ArrayList;
 
 public class Wonder {
     private boolean hasBeenBuilt;
+    private int points;
     private ArrayList<String> cost;
 
+    public Wonder(boolean hasBeenBuilt, ArrayList<String> cost)
+    {
+        this.hasBeenBuilt = hasBeenBuilt;
+        this.cost = cost;
+    }
+
+    public int getPoints()
+    {
+        return points;
+    }
+
+
+    public ArrayList<String> getCost() {
+        return cost;
+    }
+
+    public boolean isHasBeenBuilt() {
+        return hasBeenBuilt;
+    }
+
+    public String toString()
+    {
+        if(hasBeenBuilt)
+            return cost + " - built";
+        return cost + " - not built";
+    }
 }
