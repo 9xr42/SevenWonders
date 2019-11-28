4854324a9d512015171218a33f3ccfcee630fe38
AliceXu9404 <56173195+AliceXu9404@users.noreply.github.com>
2019-11-28 14:10:42 -0600

    Update Wonder.java

--- src/Wonder.java
+++ src/Wonder.java
@@ -4,18 +4,21 @@ public class Wonder {
     private boolean hasBeenBuilt;
     private int points;
     private ArrayList<String> cost;
+    private String effect;
 
-    public Wonder(boolean hasBeenBuilt, ArrayList<String> cost)
+    public Wonder(boolean hasBeenBuilt, String cost, int points, String effect)
     {
         this.hasBeenBuilt = hasBeenBuilt;
-        this.cost = cost;
+        String[] str = cost.split(" ");
+        this.cost = new ArrayList<String>();
+        for(String i: str)
+            this.cost.add(i);
     }
 
     public int getPoints()
     {
         return points;
     }
-
 
     public ArrayList<String> getCost() {
         return cost;
