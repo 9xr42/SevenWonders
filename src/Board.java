816827aac0698d13c07252ad2b32c9627788c147
AliceXu9404 <56173195+AliceXu9404@users.noreply.github.com>
2019-11-28 14:08:53 -0600

    Update Board.java

--- src/Board.java
+++ src/Board.java
@@ -1,12 +1,64 @@
-import java.io.IOException;
+import java.io.*;
+import java.util.Scanner;
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class Board {
     private Deck deck;
+    private ArrayList<Card> ageDeck;
+    private ArrayList<Card> discardPile;
+    private int age;
     private ArrayList<Player> players;
+    private int mainPlayer;
+    private boolean direction;
 
     public Board() throws IOException {
-        deck = new Deck();
+        deck = new Deck(); //////////////////////////////////
+        age = 1;
+        discardPile = new ArrayList<Card>();
+        direction = true; //true - clockwise. false - counterclockwise
+        ageDeck = deck.getAgeDeck(age);
+
+        Scanner sc = new Scanner(new File("wonderBoards.txt"));
+        ArrayList<PlayerBoard> wonders = new ArrayList<PlayerBoard>();
+        sc.nextLine();
+        while(sc.hasNextLine()) {
+            String[] str = sc.nextLine().split("|");
+            wonders.add(new PlayerBoard(str[0], new Wonder(false, str[1], 3, null),
+                    new Wonder(false, str[2], 0, str[3]),
+                    new Wonder(false, str[4], 7, null), str[5]));
+        }
+        ArrayList<Integer> wonderAssign = new ArrayList<Integer>();
+        for(int i = 1; i<11; i++)
+            wonderAssign.add(i);
+
         players = new ArrayList<Player>();
+        //for(int i = 0; i<4; i++)
+        //{
+        //    Collections.shuffle(wonderAssign);
+        //    players.add(new Player(wonders.get(wonderAssign.remove(0)), , 3, i))
+        //}
+
+    }
+
+    public void playerTurn(int index) //graphic input
+    {
+        playCard(index);
+        mainPlayer++;
+        if(mainPlayer>3)
+            mainPlayer-=4;
+    }
+
+    public void playCard(int card)
+    {
+        Card temp = players.get(mainPlayer).getHand().get(card); //gets the card in the hand
+        players.get(mainPlayer).addCard(temp);
+    }
+
+    public void incrementAge()
+    {
+        age++;
+        ageDeck = deck.getAgeDeck(age);
+        direction = !direction;
     }
 }
